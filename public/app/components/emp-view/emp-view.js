14297fe4b1cc520f4e2d066f6e13b44ef2e00eb5
Joshua Olds <joshua.olds@outlook.com>
2016-11-07 16:52:05 -0700

    Angular up and running

---
+++ public/app/components/emp-view/emp-view.js
@@ -0,0 +1,18 @@
+;(function (){
+
+    angular.module('bisualize')
+
+    .component('empView',{
+        controller: EmpViewController,
+        templateUrl: '/public/app/components/emp-view/emp-view.html'
+    })
+
+    function EmpViewController(){
+
+    }
+
+
+
+    
+
+}())
