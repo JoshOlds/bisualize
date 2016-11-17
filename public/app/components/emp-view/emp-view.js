58e664ae688363abdb90450b58631b19084dff7b
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2016-11-16 21:33:38 -0700

    updates to emp-view

--- public/app/components/emp-view/emp-view.js
+++ public/app/components/emp-view/emp-view.js
@@ -7,10 +7,18 @@
         templateUrl: '/app/components/emp-view/emp-view.html'
     })
 
+    
+    EmpViewController.$inject = ['EmpService']
+
     function EmpViewController(){
 
+        let evc = this
 
-
+        evc.getEmployees = function(){
+            EmpService.getById(function(res){
+                console.log(res)
+            })
+        }
 
 
 
