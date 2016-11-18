6ef7bb4ad2a939dc025f1759c94beb425df54700
Meredith Ganz <2meredithganz@gmail.com>
2016-11-18 15:08:39 -0700

    meredith

--- public/app/components/emp-view/emp-view.js
+++ public/app/components/emp-view/emp-view.js
@@ -1,34 +1,41 @@
-;(function (){
+; (function () {
 
     angular.module('bisualize')
 
-    .component('empView',{
-        controller: EmpViewController,
-        templateUrl: '/app/components/emp-view/emp-view.html',
-        controllerAs: 'evc'
-    })
+        .component('empView', {
+            controller: EmpViewController,
+            templateUrl: '/app/components/emp-view/emp-view.html',
+            controllerAs: 'evc'
+        })
 
-    EmpViewController.$inject = ['BisualizeService']
+    EmpViewController.$inject = ['$scope', 'BisualizeService']
 
-    function EmpViewController(BisualizeService){
+    function EmpViewController($scope, BisualizeService) {
         let evc = this
 
-        evc.getAllEmployees = function(){
+
+        function update() {
+            $scope.$evalAsync()
+        }
+
+        evc.getAllEmployees = function () {
             debugger
             BisualizeService.getEmployee('2878a62f-3a7c-4c0b-a4fc-6d6edd968b93')
-            .then(data =>{
-                console.log(data)
-            })
-            .catch(err =>{
-                console.log(err)
-            })
+                .then(data => {
+                    evc.employee = data
+                    console.log(evc.employee)
+                    update()
+                })
+                .catch(err => {
+                    console.log(err)
+                })
         }
-        
+
 
     }
 
 
 
-    
 
-}())
+
+} ())
