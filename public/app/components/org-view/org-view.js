541eef06578f274e53c434ed790de17d2bb6e25a
Joshua Olds <joshua.olds@outlook.com>
2016-11-29 17:05:49 -0700

    added tree dependencies

--- public/app/components/org-view/org-view.js
+++ public/app/components/org-view/org-view.js
@@ -1,18 +1,87 @@
-;(function (){
+; (function () {
 
     angular.module('bisualize')
 
-    .component('orgView',{
-        controller: OrgViewController,
-        templateUrl: '/app/components/org-view/org-view.html'
-    })
+        .component('orgView', {
+            controller: OrgViewController,
+            templateUrl: '/app/components/org-view/org-view.html',
+            controllerAs: 'ovc'
+        })
 
-    function OrgViewController(){
+    OrgViewController.$inject = ['$scope', 'BisualizeService']
+
+    function OrgViewController($scope, BisualizeService) {
+        let ovc = this;
+        ovc.positions = [];
+
+        BisualizeService.getAllPositions().then(data => {
+            ovc.positions = data;
+            showTree();
+        })
+
+        function showTree(){
+
+            let chartConfig = {};
+            let nodeStructure = {};
+            let toDoArr = [];
+
+            chartConfig.chart = {
+                container: "#tree-simple"
+            }
+
+            let topPosition = {};
+            ovc.positions.forEach(pos =>{
+                if(pos.managerPositionId == '-1'){
+                    topPosition = pos;
+                }
+            })
+
+            toDoArr.push(topPosition);
+
+            processChildren(toDoArr){
+                
+            }
+
+
+        }
+
+        function processChildren(toDoArray){
+
+        }
+
+        simple_chart_config = {
+            chart: {
+                container: "#tree-simple",
+                
+            },
+
+            nodeStructure: {
+                text: { name: "Parent node" },
+                children: [
+                    {
+                        text: { name: "First child" },
+                        children: [
+                            {
+                                text: {name: "Josh"}
+                            },
+                            {
+                                text: {name: "Dustin"}
+                            }
+                        ]
+                    },
+                    {
+                        text: { name: "Second child" }
+                    }
+                ]
+            }
+        };
+
+        var chart = new Treant(simple_chart_config, function () { console.log("Tree loaded.")}, $);
 
     }
 
 
 
-    
 
-}())
+
+} ())
